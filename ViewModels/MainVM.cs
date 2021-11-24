bbaf59920455b716564f0a0dc0ccff1f2357a962
Undude <72217015+SashkaPIX@users.noreply.github.com>
2021-11-24 21:48:39 +0300

    added TabControl style

--- ViewModels/MainVM.cs
+++ ViewModels/MainVM.cs
@@ -1,7 +1,5 @@
 using ReactiveUI;
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace SimpleBrowser.ViewModels
 {
@@ -11,12 +9,25 @@ namespace SimpleBrowser.ViewModels
         {
             TabVMs = new List<TabVM>();
             TabVMs.Add(new WebsiteTabVM());
+            TabVMs.Add(new WebsiteTabVM() {Name="Test tab withbigtext"});
+            TabVMs.Add(new WebsiteTabVM());
+            TabVMs.Add(new WebsiteTabVM());
         }
         private List<TabVM> _tabVMs;
         public List<TabVM> TabVMs
         {
             get { return _tabVMs; }
-            set { this.RaiseAndSetIfChanged(ref _tabVMs, value); }
+            set 
+            { 
+                this.RaiseAndSetIfChanged(ref _tabVMs, value);
+                TabItemCount = TabVMs.Count;
+            }
+        }
+        private int _tabItemCount;
+        public int TabItemCount
+        {
+            get { return _tabItemCount; }
+            set { this.RaiseAndSetIfChanged(ref _tabItemCount, value); }
         }
     }
 }
--- ViewModels/TabVM.cs
+++ ViewModels/TabVM.cs
@@ -1,10 +1,21 @@
-﻿using SimpleBrowser.Translations;
+﻿using ReactiveUI;
+using SimpleBrowser.Translations;
 
 namespace SimpleBrowser.ViewModels
 {
     public class TabVM : ViewModelBase
     {
-        public string Name { get; set; }
-        public bool IsSelected { get; set; }
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { this.RaiseAndSetIfChanged(ref _name, value); }
+        }
+        private bool _isSelected;
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set { this.RaiseAndSetIfChanged(ref _isSelected, value); }
+        }
     }
 }
